e8a55feffb92c9718d91801bb4ffb3e24e3b5225
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-26 02:52:19 -0500

    Update test_log_correlation.py

--- app/tests/test_log_correlation.py
+++ app/tests/test_log_correlation.py
@@ -1,1 +1,15 @@
+# /tests/test_log_correlation.py
+import unittest
+from app.core.log_correlation import correlate_logs
+
+class TestLogCorrelation(unittest.TestCase):
+    def test_correlate_logs(self):
+        # Placeholder test for log correlation
+        normalized_data = [["error", "123"], ["warning", "456"], ["info", "789"]]
+        correlated_data = correlate_logs(normalized_data)
+        self.assertIsNotNone(correlated_data)
+        self.assertGreater(len(correlated_data), 0)
+
+if __name__ == '__main__':
+    unittest.main()
 

ce2d125c76c799b73cefbc5eef95faf5074c84a5
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-26 02:52:30 -0500

    Update tests_log_parser.py

--- app/tests/tests_log_parser.py
+++ app/tests/tests_log_parser.py
@@ -1,1 +1,15 @@
+# /tests/test_log_parser.py
+import unittest
+from app.core.log_parser import parse_log
+
+class TestLogParser(unittest.TestCase):
+    def test_parse_log(self):
+        # Placeholder test for log parsing
+        log_file_path = "sample.log"
+        log_entries = parse_log(log_file_path)
+        self.assertIsNotNone(log_entries)
+        self.assertGreater(len(log_entries), 0)
+
+if __name__ == '__main__':
+    unittest.main()
 

c6cd59782a10203d263b69734bc43238fbb023f4
Artemis <113257055+Artemis-solomon@users.noreply.github.com>
2023-11-26 02:54:16 -0500

    Update log_normalizer.py

--- app/core/log_normalizer.py
+++ app/core/log_normalizer.py
@@ -1,1 +1,6 @@
+# /app/core/log_normalizer.py
+def normalize_log(log_data):
+    # Add your implementation for log normalization here
+    # This can include code to standardize the representation of log entries
+    pass
 
